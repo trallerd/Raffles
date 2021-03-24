34c6d0771e73699a45ce9fe1a4e9031e22d61ccd
trallerd <jessicacristec@gmail.com>
2021-03-24 16:36:25 -0300

    Zip Exemplo Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_183922) do
+ActiveRecord::Schema.define(version: 2021_03_24_193426) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.boolean "active"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.integer "agr"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
