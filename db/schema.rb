b63fb222066055a7ac3662e1a182f90b4a036838
trallerd <jessicacristec@gmail.com>
2021-03-24 15:46:37 -0300

    migrate companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_175139) do
+ActiveRecord::Schema.define(version: 2021_03_24_183922) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.boolean "active"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "kinds", force: :cascade do |t|
     t.string "description"
