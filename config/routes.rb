34274d7619f7c5ad10b49b239ae108361df83442
trallerd <jessicacristec@gmail.com>
2021-03-26 18:45:05 -0300

    Configure route root

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :raffles
   resources :users
   resources :types
+  root to: 'raffles#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
